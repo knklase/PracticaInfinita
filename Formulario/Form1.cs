722a7364aed864baf64760d1545f479488d6c2da
JMBPracticando <99740110+JMBPracticando@users.noreply.github.com>
2022-02-22 19:32:50 +0100

    El codigo del boton

--- Formulario/Form1.cs
+++ Formulario/Form1.cs
@@ -16,5 +16,14 @@ namespace Formulario
         {
             InitializeComponent();
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            tListaSocios.Text += tNombre.Text + " " + tApellidos.Text + " " + tTelefono.Text + "\r\n";
+            tNombre.Clear();
+            tApellidos.Clear();
+            tTelefono.Clear();
+            tNombre.Focus();
+        }
     }
 }
